a4eb33be6c5037f9e1af268a716af4593ce97a5b
Yakovlev Mikhail Alekseevich <yakovmikemikle@gmail.com>
2022-08-31 09:52:36 +0300

    deploy to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ed_2022/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
