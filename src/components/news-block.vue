ba87fc25e5246141fc69a570bcb79029a10b73aa
nik-leppenen@yandex.ru <nik-leppenen@yandex.ru>
2022-12-13 21:59:25 +0200

    add news

--- src/components/news-block.vue
+++ src/components/news-block.vue
@@ -8,36 +8,8 @@ import LinkWrapper from './ui/link-wrapper.vue';
 <template>
   <PageBlock
     title='Новости'
-  >
-    <span>
-      <img
-        :class='$style.file'
-        :src='file'
-      >&nbsp;
-    </span><LinkWrapper
-      :href='test2a'
-      :target='"_blank"'
-    >
-      Решение контрольной №2
-    </LinkWrapper>
-    <div :class='$style.heading'>Критерии оценивания:</div>
-    <div :class='$style.task'>
-      <div :class='$style.taskName'>1 задача:</div>
-      <ol type='a'>
-        <li>Найдены показатели преломления (по 2 балла за каждую поляризацию)</li>
-        <li>Найдены условия отсутствия отражения (по 3 балла за каждую поляризацию)</li>
-      </ol>
-    </div>
-    <div :class='$style.task'>
-      <div :class='$style.taskName'>2 задача:</div>
-      <ol type='a'>
-        <li>Поле E в СО пластинки (2 балла)</li>
-        <li>Поляризация P в СО пластинки (2 балла)</li>
-        <li>E в СО пластинки (1 балла)</li>
-        <li>E в лабораторной СО/ матрица поворорта для матрицы плотности (2 балла)</li>
-        <li>Параметры Стокса (по баллу за каждый)</li>
-      </ol>
-    </div>
+  Последняя лекция курса — 19 декабря в 16:00.
+  Контрольная работа по третьей части курса состоится 20 декабря в 12:00
   </PageBlock>
 </template>
 
