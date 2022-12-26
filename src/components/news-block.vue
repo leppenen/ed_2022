547734603d16c2866bef96495fa09184b6c7689e
Yakovlev Mikhail Alekseevich <mikhayakovlev@ozon.ru>
2022-12-26 21:11:43 +0400

    test 3 news

--- src/components/news-block.vue
+++ src/components/news-block.vue
@@ -1,13 +1,44 @@
 <script setup lang='ts'>
 import PageBlock from '@/components/ui/page-block.vue'
+import test3a from '../assets/files/tasks/test3a.pdf'
+import file from '../assets/images/file.svg'
+import LinkWrapper from './ui/link-wrapper.vue';
 </script>
 
 <template>
   <PageBlock
     title='Новости'
   >
-    <p>Последняя лекция курса — 19 декабря в 16:00</p>
-    <p>Контрольная работа по третьей части курса состоится 20 декабря в <span :class='$style.highlighted'>12:00</span></p>
+    <span>
+      <img
+        :class='$style.file'
+        :src='file'
+      >&nbsp;
+    </span><LinkWrapper
+      :href='test3a'
+      :target='"_blank"'
+    >
+      Решение контрольной №3
+    </LinkWrapper>
+    <div :class='$style.heading'>Критерии оценивания:</div>
+    <div :class='$style.task'>
+      <div :class='$style.taskName'>Задача 1:</div>
+      <ol type='a'>
+        <li>Получен амплитудный коэффициент прохождения через всю структуру (2 балла)</li>
+        <li>Получено условие на максимум с учетом фазы коэффициентов отражения (2 балла) -- если про фазы ничего не сказано (1 балл)</li>
+        <li>Получено выражения для модуля через t и r (2 балла)</li>
+        <li>Ответ упрощен с учетом условий |t_{1,2}|&#60;&#60;1 (2 балла)</li>
+      </ol>
+    </div>
+    <div :class='$style.task'>
+      <div :class='$style.taskName'>Задача 2:</div>
+      <ol type='a'>
+        <li>Получена матрица переноса через слой с ямой (2 балла)</li>
+        <li>Верно получены r и t в резонансе для одной ямы (по 1 баллу)</li>
+        <li>Получены выражения для r и t для структуры из двух ям (по 1 баллу)</li>
+        <li>Верно получены r и t в резонансе для двух ям (по 1 баллу)</li>
+      </ol>
+    </div>
   </PageBlock>
 </template>
 
@@ -26,8 +57,5 @@ import PageBlock from '@/components/ui/page-block.vue'
 }
 .task > ol {
   padding-left: 24px;
-}
-.highlighted {
-  color: var(--textSecondary);
 }
 </style>
